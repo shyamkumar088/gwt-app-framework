6ab2a2cf0f6b1f15f47d27e9315825b793e99756
akalmenson <akalmenson@mtsinai.on.ca>
2009-07-15 12:39:09 +0000

    * initial creation of the gwt-af-sample module. * this module will be used to demonstrate how the gwt app framework library works.

---
+++ gwt-af-sample/src/main/java/org/gwtaf/client/Sample.java
@@ -0,0 +1,13 @@
+package org.gwtaf.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Sample implements EntryPoint {
+
+	public void onModuleLoad() {
+
+	}
+}
