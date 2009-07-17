c4934ee73d175f17676a2daff3f9563b8bfb0d30
akalmenson <akalmenson@mtsinai.on.ca>
2009-07-17 18:53:35 +0000

    * added a Widget implementation similar to the data structure UiBundle.
    Issue Addressing: CMS-34

---
+++ gwt-af-widgets/src/main/java/org/gwtaf/widgets/generic/UiBundleWidget.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2008. Mount Sinai Hospital, Toronto, Canada.
+ * 
+ * Licensed under the Apache License, Version 2.0. You
+ * can find a copy of the license at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * IN NO EVENT SHALL MOUNT SINAI HOSPITAL BE LIABLE TO ANY PARTY FOR DIRECT, 
+ * INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST 
+ * PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, 
+ * EVEN IF MOUNT SINAI HOSPITAL HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH 
+ * DAMAGE.
+ * 
+ * MOUNT SINAI HOSPITAL SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE AND 
+ * ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED "AS IS". 
+ * MOUNT SINAI HOSPITAL HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, 
+ * UPDATES, ENHANCEMENTS, OR MODIFICATIONS. 
+ */
+package org.gwtaf.widgets.generic;
+
+import org.gwtaf.widgets.UiBundle;
+
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * This class is along a similar idea as {@link UiBundle} except instead of just
+ * being a data structure, this is an actual GWT {@link Widget}.
+ * 
+ * @author Arthur Kalmenson
+ */
+public class UiBundleWidget<L extends Widget, W extends Widget> extends
+		Composite {
+
+	/**
+	 * Create a new <code>UiBundleWidget</code> with the given Panel, L and W
+	 * type widgets.
+	 * 
+	 * @param panel
+	 *            the panel to add to.
+	 * @param label
+	 *            the label widget.
+	 * @param widget
+	 *            the widget.
+	 */
+	public UiBundleWidget(Panel panel, L label, W widget) {
+		panel.add(label);
+		panel.add(widget);
+	}
+}
---
+++ gwt-af-widgets/src/test/java/org/gwtaf/widgets/generic/UiBundleWidgetTest.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2008. Mount Sinai Hospital, Toronto, Canada.
+ * 
+ * Licensed under the Apache License, Version 2.0. You
+ * can find a copy of the license at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * IN NO EVENT SHALL MOUNT SINAI HOSPITAL BE LIABLE TO ANY PARTY FOR DIRECT, 
+ * INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST 
+ * PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, 
+ * EVEN IF MOUNT SINAI HOSPITAL HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH 
+ * DAMAGE.
+ * 
+ * MOUNT SINAI HOSPITAL SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE AND 
+ * ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED "AS IS". 
+ * MOUNT SINAI HOSPITAL HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, 
+ * UPDATES, ENHANCEMENTS, OR MODIFICATIONS. 
+ */
+package org.gwtaf.widgets.generic;
+
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.google.gwt.junit.GWTMockUtilities;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * Test the {@link UiBundleWidget} class.
+ * 
+ * @author Arthur Kalmenson
+ */
+public class UiBundleWidgetTest {
+
+	/**
+	 * A mock of a {@link Panel}.
+	 */
+	@Mock
+	private Panel panelMock;
+
+	/**
+	 * A mock of the label.
+	 */
+	@Mock
+	private Label labelMock;
+
+	/**
+	 * A mock of a textbox.
+	 */
+	@Mock
+	private Widget widgetMock;
+
+	/**
+	 * The {@link UiBundleWidget} we're testing.
+	 */
+	private UiBundleWidget<Label, Widget> uiBundleWidget;
+
+	@BeforeClass
+	public void disarmGwt() {
+		GWTMockUtilities.disarm();
+	}
+
+	@AfterClass
+	public void rearmGwt() {
+		GWTMockUtilities.restore();
+	}
+
+	@BeforeMethod
+	public void initBefore() {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	/**
+	 * Return a list of bad constructor arguments.
+	 * 
+	 * @return a list of bad constructor arguments.
+	 */
+	@DataProvider(name = "badArgs")
+	public Object[][] badConstructorArgProvider() {
+		return new Object[][] { { panelMock, null, null },
+				{ null, labelMock, null }, { panelMock, null, widgetMock } };
+	}
+
+	/**
+	 * Test creating a {@link UiBundleWidget} with bad constructor arguments. We
+	 * expect a {@link NullPointerException} for nulls.
+	 * 
+	 * @param panel
+	 *            the {@link Panel}.
+	 * @param label
+	 *            the {@link Label}.
+	 * @param widget
+	 *            the {@link Widget}.
+	 */
+	@Test(expectedExceptions = NullPointerException.class, dataProvider = "badArgs")
+	public void badConstructorArgs(Panel panel, Label label, Widget widget) {
+		uiBundleWidget = new UiBundleWidget<Label, Widget>(panel, label, widget);
+	}
+
+	/**
+	 * Test to ensure the label and widget are added to the Panel that's given.
+	 * We also want to ensure the label is added before the widget.
+	 */
+	@Test
+	public void layoutCheck() {
+
+		// create it.
+		uiBundleWidget = new UiBundleWidget<Label, Widget>(panelMock,
+				labelMock, widgetMock);
+
+		// verify they were added.
+		InOrder inOrder = inOrder(panelMock);
+		inOrder.verify(panelMock).add(labelMock);
+		inOrder.verify(panelMock).add(widgetMock);
+
+		// verify there was no other action.
+		verifyNoMoreInteractions(panelMock);
+		verifyZeroInteractions(labelMock);
+		verifyZeroInteractions(widgetMock);
+	}
+}
