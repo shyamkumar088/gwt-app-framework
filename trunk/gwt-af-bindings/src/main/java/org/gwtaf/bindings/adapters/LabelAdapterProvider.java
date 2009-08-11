ece67443fe41f33c5d5fe1c4d20a7cdbe6d889d3
svesselov@mtsinai.on.ca <svesselov@mtsinai.on.ca@806756f2-60bb-11de-9789-710e3b6db069>
2009-08-11 17:50:29 +0000

    * Set proper license
    Issue Addressing: CMS-59

--- trunk/gwt-af-bindings/src/main/java/org/gwtaf/bindings/adapters/LabelAdapterProvider.java
+++ trunk/gwt-af-bindings/src/main/java/org/gwtaf/bindings/adapters/LabelAdapterProvider.java
@@ -1,6 +1,11 @@
 /*
  * Copyright 2009. Mount Sinai Hospital, Toronto, Canada.
  * 
+ * Licensed under the Apache License, Version 2.0. You
+ * can find a copy of the license at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * IN NO EVENT SHALL MOUNT SINAI HOSPITAL BE LIABLE TO ANY PARTY FOR DIRECT, 
  * INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST 
  * PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, 
