a560bbe442959857b2beb432a5ea54fdbe558035
akalmenson <akalmenson@mtsinai.on.ca>
2009-08-12 18:30:21 +0000

    * added an EventBus wrapper interface and a concrete implementation that just uses HandlerManager. * added an AbstractGinModule to bind it. * renamed the GWT module file.
    Issue Addressed: CMS-58

---
+++ gwt-af-eventbus/src/main/java/org/gwtaf/eventbus/HandlerManagerBus.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2008. Mount Sinai Hospital, Toronto, Canada.
+ * 
+ * Licensed under the Apache License, Version 2.0. You
+ * can find a copy of the license at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * IN NO EVENT SHALL MOUNT SINAI HOSPITAL BE LIABLE TO ANY PARTY FOR DIRECT, 
+ * INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST 
+ * PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, 
+ * EVEN IF MOUNT SINAI HOSPITAL HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH 
+ * DAMAGE.
+ * 
+ * MOUNT SINAI HOSPITAL SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE AND 
+ * ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED "AS IS". 
+ * MOUNT SINAI HOSPITAL HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, 
+ * UPDATES, ENHANCEMENTS, OR MODIFICATIONS. 
+ */
+package org.gwtaf.eventbus;
+
+import com.google.gwt.event.shared.HandlerManager;
+
+/**
+ * A concrete implementation of {@link EventBus} using the
+ * {@link HandlerManager} as the base without any customizations.
+ * 
+ * @author Arthur Kalmenson
+ */
+public class HandlerManagerBus extends HandlerManager implements EventBus {
+
+	/**
+	 * Creates a new <code>HandlerManagerBus</code> with a null source.
+	 */
+	public HandlerManagerBus() {
+		super(null);
+	}
+}
