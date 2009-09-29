51063a9158ab07721c025bb5f009cb753ff5a68c
akalmenson <akalmenson@mtsinai.on.ca>
2009-09-29 20:51:57 +0000

    * added the interfaces required for the Command pattern as well as the rpc services for executing commands.
    Issue Addressing: CMS-83

---
+++ gwt-af-command/src/main/java/org/gwtaf/command/ActionHandler.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2009. Mount Sinai Hospital, Toronto, Canada.
+ * 
+ * IN NO EVENT SHALL MOUNT SINAI HOSPITAL BE LIABLE TO ANY PARTY FOR DIRECT, 
+ * INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST 
+ * PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, 
+ * EVEN IF MOUNT SINAI HOSPITAL HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH 
+ * DAMAGE.
+ * 
+ * MOUNT SINAI HOSPITAL SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE AND 
+ * ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED "AS IS". 
+ * MOUNT SINAI HOSPITAL HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, 
+ * UPDATES, ENHANCEMENTS, OR MODIFICATIONS. 
+ */
+package org.gwtaf.command;
+
+/**
+ * A class that executes specific {@link Action}s and returns the
+ * {@link Response} to that action.
+ * 
+ * @author Arthur Kalmenson
+ * 
+ * @param <A>
+ *            the {@link Action} being handled.
+ * @param <R>
+ *            the {@link Response} to that Action.
+ */
+public interface ActionHandler<A extends Action<R>, R extends Response> {
+
+	/**
+	 * Executes the {@link Action} and returns a {@link Response} to that
+	 * action.
+	 * 
+	 * @param action
+	 *            the {@link Action} to execute.
+	 * @return the {@link Response} returned.
+	 */
+	R execute(A action);
+}
---
+++ gwt-af-command/src/main/java/org/gwtaf/command/rpc/CommandService.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2009. Mount Sinai Hospital, Toronto, Canada.
+ * 
+ * IN NO EVENT SHALL MOUNT SINAI HOSPITAL BE LIABLE TO ANY PARTY FOR DIRECT, 
+ * INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST 
+ * PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, 
+ * EVEN IF MOUNT SINAI HOSPITAL HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH 
+ * DAMAGE.
+ * 
+ * MOUNT SINAI HOSPITAL SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE AND 
+ * ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED "AS IS". 
+ * MOUNT SINAI HOSPITAL HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, 
+ * UPDATES, ENHANCEMENTS, OR MODIFICATIONS. 
+ */
+package org.gwtaf.command.rpc;
+
+import org.gwtaf.command.Action;
+import org.gwtaf.command.Response;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+
+/**
+ * The GWT {@link RemoteService} that executes {@link Action} items.
+ * 
+ * @author Arthur Kalmenson
+ */
+public interface CommandService extends RemoteService {
+
+	/**
+	 * Execute the given {@link Action} and return a corresponding
+	 * {@link Response}.
+	 * 
+	 * @param <R>
+	 *            the type of {@link Response}.
+	 * @param action
+	 *            the {@link Action} to execute.
+	 * @return a {@link Response} to the {@link Action} being executed.
+	 */
+	<R extends Response> R execute(Action<R> action);
+}
