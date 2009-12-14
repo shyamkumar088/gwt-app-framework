3a7a8b43b020d23280b72eab367d0a164e2accd7
jkong <jkong@mtsinai.on.ca>
2009-12-14 19:07:20 +0000

    * Turned this into an Abstract class instead of an interface  * Classes will need to extend this instead of implementing it
     Issue Addressed: ISHAKE-54

--- gwt-af-eventbus/src/main/java/org/gwtaf/eventbus/rpc/RpcEventCreatorService.java
+++ gwt-af-eventbus/src/main/java/org/gwtaf/eventbus/rpc/RpcEventCreatorService.java
@@ -20,6 +20,18 @@
  */
 package org.gwtaf.eventbus.rpc;
 
+import org.gwtaf.command.shared.Action;
+import org.gwtaf.command.shared.Response;
+import org.gwtaf.eventbus.EventBus;
+import org.gwtaf.eventbus.event.ErrorEvent;
+import org.gwtaf.eventbus.event.RpcEvent;
+import org.gwtaf.eventbus.event.RpcEventHandler;
+import org.gwtaf.eventbus.event.RpcResponseEvent;
+
+import com.google.gwt.event.shared.GwtEvent.Type;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.inject.Provider;
+
 /**
  * An interface for a service that will be responsible for creating
  * {@link RpcEvent}s.
@@ -27,6 +39,84 @@ package org.gwtaf.eventbus.rpc;
  * @author Jason Kong
  * 
  */
-public interface RpcEventCreatorService {
+public abstract class RpcEventCreatorService {
+
+	/**
+	 * The {@link EventBus} used for event communication
+	 */
+	protected EventBus eventBus;
+
+	/**
+	 * The {@link Provider} of {@link ErrorEvent}s
+	 */
+	protected Provider<ErrorEvent> errorEventProvider;
+
+	/**
+	 * The {@link Type} associated with something that handles {@link RpcEvent}s
+	 */
+	protected Type<RpcEventHandler> rpcHandlerType;
+
+	/**
+	 * Constructor that all extending classes will have to call. For now, simply
+	 * ensures that all the fields in the abstract classes exist
+	 * 
+	 * @param eventBus
+	 *            the {@link EventBus}
+	 * @param errorEventProvider
+	 *            the {@link Provider} of {@link ErrorEvent}s
+	 * @param rpcHandlerType
+	 *            the {@link Type} associated with rpc event handler
+	 */
+	public RpcEventCreatorService(EventBus eventBus,
+			Provider<ErrorEvent> errorEventProvider,
+			Type<RpcEventHandler> rpcHandlerType) {
+
+		this.eventBus = eventBus;
+		this.errorEventProvider = errorEventProvider;
+		this.rpcHandlerType = rpcHandlerType;
+
+	}
+
+	/**
+	 * Performs the standard RPC firing. Takes in an {@link Action}, builds the
+	 * default {@link AsyncCallback} (using the specified RpcResponseEvent
+	 * provider) and fires off the {@link RpcEvent}
+	 * 
+	 * @param <R>
+	 *            The {@link Response} type
+	 * @param <A>
+	 *            The {@link Action} type
+	 * @param action
+	 *            The actual {@link Action} command to execute
+	 * @param provider
+	 *            the {@link Provider} for getting {@link RpcResponseEvent}s to
+	 *            fire back on callback success
+	 */
+	protected <R extends Response, A extends Action<R>> void fireStandardRpc(
+			A action, final Provider<RpcResponseEvent<R>> provider) {
+
+		// build a callback
+		AsyncCallback<R> callback = new AsyncCallback<R>() {
+
+			public void onFailure(Throwable arg0) {
+				eventBus.fireEvent(errorEventProvider.get());
+			}
+
+			public void onSuccess(R response) {
+				RpcResponseEvent<R> rpcResponseEvent = provider.get();
+				rpcResponseEvent.setResponse(response);
+				eventBus.fireEvent(rpcResponseEvent);
+			}
+
+		};
+
+		// fire off the rpc event for search
+		RpcEvent<A, AsyncCallback<R>, R> rpcEvent = new RpcEvent<A, AsyncCallback<R>, R>(
+				rpcHandlerType);
+
+		rpcEvent.setActionAndCallback(action, callback);
+		eventBus.fireEvent(rpcEvent);
+
+	}
 
 }
