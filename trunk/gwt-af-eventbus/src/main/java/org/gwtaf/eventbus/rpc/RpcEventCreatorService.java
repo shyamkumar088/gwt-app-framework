47117d0e9ac9488eafcc1056c2d4e6bcdfecd9a1
jkong@mtsinai.on.ca <jkong@mtsinai.on.ca@806756f2-60bb-11de-9789-710e3b6db069>
2009-12-15 14:00:59 +0000

    * Renamed the abstract class to AbstractRpcEventCreator  * "Restored" the RpcEventCreatorService interface
     Issue Addressed: ISHAKE-55

--- trunk/gwt-af-eventbus/src/main/java/org/gwtaf/eventbus/rpc/RpcEventCreatorService.java
+++ trunk/gwt-af-eventbus/src/main/java/org/gwtaf/eventbus/rpc/RpcEventCreatorService.java
@@ -1,10 +1,5 @@
 /*
- * Copyright 2008. Mount Sinai Hospital, Toronto, Canada.
- * 
- * Licensed under the Apache License, Version 2.0. You
- * can find a copy of the license at:
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
+ * Copyright 2009. Mount Sinai Hospital, Toronto, Canada.
  * 
  * IN NO EVENT SHALL MOUNT SINAI HOSPITAL BE LIABLE TO ANY PARTY FOR DIRECT, 
  * INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST 
@@ -20,17 +15,7 @@
  */
 package org.gwtaf.eventbus.rpc;
 
-import org.gwtaf.command.shared.Action;
-import org.gwtaf.command.shared.Response;
-import org.gwtaf.eventbus.EventBus;
-import org.gwtaf.eventbus.event.ErrorEvent;
 import org.gwtaf.eventbus.event.RpcEvent;
-import org.gwtaf.eventbus.event.RpcEventHandler;
-import org.gwtaf.eventbus.event.RpcResponseEvent;
-
-import com.google.gwt.event.shared.GwtEvent.Type;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.inject.Provider;
 
 /**
  * An interface for a service that will be responsible for creating
@@ -39,84 +24,6 @@ import com.google.inject.Provider;
  * @author Jason Kong
  * 
  */
-public abstract class RpcEventCreatorService {
-
-	/**
-	 * The {@link EventBus} used for event communication
-	 */
-	protected EventBus eventBus;
-
-	/**
-	 * The {@link Provider} of {@link ErrorEvent}s
-	 */
-	protected Provider<ErrorEvent> errorEventProvider;
-
-	/**
-	 * The {@link Type} associated with something that handles {@link RpcEvent}s
-	 */
-	protected Type<RpcEventHandler> rpcHandlerType;
-
-	/**
-	 * Constructor that all extending classes will have to call. For now, simply
-	 * ensures that all the fields in the abstract classes exist
-	 * 
-	 * @param eventBus
-	 *            the {@link EventBus}
-	 * @param errorEventProvider
-	 *            the {@link Provider} of {@link ErrorEvent}s
-	 * @param rpcHandlerType
-	 *            the {@link Type} associated with rpc event handler
-	 */
-	public RpcEventCreatorService(EventBus eventBus,
-			Provider<ErrorEvent> errorEventProvider,
-			Type<RpcEventHandler> rpcHandlerType) {
-
-		this.eventBus = eventBus;
-		this.errorEventProvider = errorEventProvider;
-		this.rpcHandlerType = rpcHandlerType;
-
-	}
-
-	/**
-	 * Performs the standard RPC firing. Takes in an {@link Action}, builds the
-	 * default {@link AsyncCallback} (using the specified RpcResponseEvent
-	 * provider) and fires off the {@link RpcEvent}
-	 * 
-	 * @param <R>
-	 *            The {@link Response} type
-	 * @param <A>
-	 *            The {@link Action} type
-	 * @param action
-	 *            The actual {@link Action} command to execute
-	 * @param provider
-	 *            the {@link Provider} for getting {@link RpcResponseEvent}s to
-	 *            fire back on callback success
-	 */
-	protected <R extends Response, A extends Action<R>> void fireStandardRpc(
-			A action, final Provider<RpcResponseEvent<R>> provider) {
-
-		// build a callback
-		AsyncCallback<R> callback = new AsyncCallback<R>() {
-
-			public void onFailure(Throwable arg0) {
-				eventBus.fireEvent(errorEventProvider.get());
-			}
-
-			public void onSuccess(R response) {
-				RpcResponseEvent<R> rpcResponseEvent = provider.get();
-				rpcResponseEvent.setResponse(response);
-				eventBus.fireEvent(rpcResponseEvent);
-			}
-
-		};
-
-		// fire off the rpc event for search
-		RpcEvent<A, AsyncCallback<R>, R> rpcEvent = new RpcEvent<A, AsyncCallback<R>, R>(
-				rpcHandlerType);
-
-		rpcEvent.setActionAndCallback(action, callback);
-		eventBus.fireEvent(rpcEvent);
-
-	}
+public interface RpcEventCreatorService {
 
 }
