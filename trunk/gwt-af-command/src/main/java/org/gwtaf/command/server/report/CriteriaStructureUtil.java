3bda37c8660712aaaa09d6805f511cf2aeb94609
svesselov@mtsinai.on.ca <svesselov@mtsinai.on.ca@806756f2-60bb-11de-9789-710e3b6db069>
2010-04-30 18:37:51 +0000

    * added logic for creating Criteria objects out of user filter requests.

--- trunk/gwt-af-command/src/main/java/org/gwtaf/command/server/report/CriteriaStructureUtil.java
+++ trunk/gwt-af-command/src/main/java/org/gwtaf/command/server/report/CriteriaStructureUtil.java
@@ -3,7 +3,16 @@ package org.gwtaf.command.server.report;
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+import org.gwtaf.command.server.report.CriteriaRequest.Comparators;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.ProjectionList;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
 
 /**
  * This class will parse a Domain Object structure using reflections and store
@@ -77,6 +86,9 @@ import java.util.HashMap;
 public class CriteriaStructureUtil {
 
 	private HashMap<String, String> fieldToPath;
+	private HashMap<String, String> descriptionToField;
+
+	private ArrayList<String> instantiatedAliases = new ArrayList<String>();
 
 	/**
 	 * Parses the structure of the object, constructing a Criteria dot path for
@@ -98,10 +110,14 @@ public class CriteriaStructureUtil {
 		// process the class
 		processClass(packageRestriction, obj.getClass(), "");
 
+		// create the human-readable look up names
+		createLookUpNames();
+
 		return fieldToPath;
 	}
 
-	private void processClass(String packageRestriction, Class c, String currentDotPath) {
+	private void processClass(String packageRestriction, Class c,
+			String currentDotPath) {
 
 		String className = c.getSimpleName();
 
@@ -118,8 +134,8 @@ public class CriteriaStructureUtil {
 				continue;
 			}
 
-			// if we are dealing with a Something<OurObject> use "OurObject" instead
-			// of "Something"
+			// if we are dealing with a Something<OurObject> use "OurObject"
+			// instead of "Something"
 			if (f.getGenericType() instanceof ParameterizedType) {
 				ParameterizedType type = (ParameterizedType) f.getGenericType();
 				Type[] rawTypes = type.getActualTypeArguments();
@@ -156,5 +172,151 @@ public class CriteriaStructureUtil {
 
 	public HashMap<String, String> getMap() {
 		return fieldToPath;
+	}
+
+	/**
+	 * Helper for creating human-readable field descriptors.
+	 * 
+	 * This will go through Strings like : PatientInfo.lastName
+	 * 
+	 * and turn them into strings like: Last Name from Patient Info.
+	 */
+	private void createLookUpNames() {
+
+		descriptionToField = new HashMap<String, String>();
+
+		Iterator<String> it = fieldToPath.keySet().iterator();
+		while (it.hasNext()) {
+			String classField = it.next();
+			String fullDescription = constructDescription(classField);
+			descriptionToField.put(fullDescription, classField);
+		}
+	}
+
+	protected String constructDescription(String classField) {
+		// isolate the field
+		String[] split = classField.split("\\.");
+		String field = split[1];
+		String className = split[0];
+
+		// break apart camel notation
+		field = field.replaceAll("([A-Z])", " $1");
+		className = className.replaceAll("([A-Z])", " $1");
+
+		// bring to upper case the first character.
+		String caps = field.toUpperCase();
+		field = field.replaceAll("^.", "" + caps.charAt(0));
+
+		// store the full description.
+		String fullDescription = field.trim() + " from " + className.trim();
+		return fullDescription;
+	}
+
+	/**
+	 * Fills a criteria based on the given list of filtering requests
+	 * 
+	 * @param crit
+	 *            the criteria to fill.
+	 * @param requests
+	 *            requests
+	 */
+	public void fillCriteria(Criteria crit, List<CriteriaRequest> requests) {
+
+		// clear old aliases
+		instantiatedAliases.clear();
+
+		// projection list for the data
+		ProjectionList projList = Projections.projectionList();
+
+		for (CriteriaRequest request : requests) {
+			String path = fieldToPath.get(request.getField());
+
+			// requested unknown field.
+			if (path == null) {
+				continue;
+			}
+
+			parseDotPath(crit, path, request, projList);
+		}
+
+		// assign the projection list
+		crit.setProjection(projList);
+	}
+
+	/**
+	 * Helper for stuffing aliases, projections and restrictions into a criteria
+	 * for a single request.
+	 */
+	private void parseDotPath(Criteria crit, String dotPath,
+			CriteriaRequest request, ProjectionList projList) {
+
+		String[] tokens = dotPath.split("\\.");
+		if (tokens.length == 0) {
+			tokens = new String[] { dotPath };
+		}
+
+		// handle aliases.
+		String previousToken = null;
+		for (int i = 0; i < tokens.length - 1; i++) {
+
+			// if it's the first
+			String regPath = (previousToken == null) ? tokens[i]
+					: previousToken + "." + tokens[i];
+
+			// register the alias
+			if (!instantiatedAliases.contains(tokens[i])) {
+				instantiatedAliases.add(tokens[i]);
+				crit.createAlias(regPath, tokens[i]);
+			}
+
+			// store old token
+			previousToken = tokens[i];
+		}
+
+		// handle projection
+		String field = tokens[tokens.length - 1];
+		String fieldProjection = (previousToken == null) ? field
+				: previousToken + "." + field;
+
+		// project the field
+		projList.add(Projections.property(fieldProjection));
+
+		// handle restrictions
+		createRestriction(crit, fieldProjection, request);
+	}
+
+	/**
+	 * Helper for creating restrictions of the fillCriteria() method.
+	 */
+	private void createRestriction(Criteria crit, String fieldProjection,
+			CriteriaRequest request) {
+		Comparators comp = request.getComparator();
+		String data = request.getData();
+
+		// handle all the humanly readable comparators into restrictions
+		if (comp == Comparators.EQUAL || comp == Comparators.ON
+				|| comp == Comparators.IS) {
+			crit.add(Restrictions.eq(fieldProjection, data));
+		} else if (comp == Comparators.NOT_EQUAL || comp == Comparators.NOT_ON
+				|| comp == Comparators.IS_NOT) {
+			crit.add(Restrictions.ne(fieldProjection, data));
+		} else if (comp == Comparators.AFTER || comp == Comparators.MORE_THAN) {
+			crit.add(Restrictions.gt(fieldProjection, data));
+		} else if (comp == Comparators.MORE_THAN_OR_EQUAL) {
+			crit.add(Restrictions.ge(fieldProjection, data));
+		} else if (comp == Comparators.BEFORE || comp == Comparators.LESS_THAN) {
+			crit.add(Restrictions.lt(fieldProjection, data));
+		} else if (comp == Comparators.LESS_THAN_OR_EQUAL) {
+			crit.add(Restrictions.le(fieldProjection, data));
+		} else if (comp == Comparators.CONTAINS) {
+			crit.add(Restrictions.like(fieldProjection, "%" + data + "%"));
+		} else if (comp == Comparators.DOES_NOT_CONTAIN) {
+			crit.add(Restrictions.not(Restrictions.like(fieldProjection, "%"
+					+ data + "%")));
+		} else if (comp == Comparators.STARTS_WITH) {
+			crit.add(Restrictions.like(fieldProjection, data + "%"));
+		} else if (comp == Comparators.ENDS_WITH) {
+			crit.add(Restrictions.like(fieldProjection, "%" + data));
+		}
 	}
 }
--- trunk/gwt-af-command/src/test/java/org/gwtaf/command/server/report/CriteriaDoParserTest.java
+++ trunk/gwt-af-command/src/test/java/org/gwtaf/command/server/report/CriteriaDoParserTest.java
@@ -1,11 +1,21 @@
 package org.gwtaf.command.server.report;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
+import org.gwtaf.command.server.report.CriteriaRequest.Comparators;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Projection;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import static org.mockito.Mockito.*;
 
 /**
  * Test for the CriteriaDoParser.
@@ -13,6 +23,14 @@ import org.testng.annotations.Test;
  * @author serge
  */
 public class CriteriaDoParserTest {
+
+	@Mock
+	Criteria crit;
+
+	@BeforeTest
+	public void beforeTest() {
+		MockitoAnnotations.initMocks(this);
+	}
 
 	@Test
 	public void parsingTest() {
@@ -37,6 +55,56 @@ public class CriteriaDoParserTest {
 		Assert.assertTrue(map.get("ObjectB.name").equals("b.name"));
 		Assert.assertTrue(map.get("ObjectC.endOfTheWorld").equals(
 				"b.endOfTheWorldCounter.endOfTheWorld"));
+	}
+
+	@Test
+	public void testFillCriteria() {
+		CriteriaStructureUtil parser = new CriteriaStructureUtil();
+		parser.parseStructure(new ObjectA(), "org.gwtaf.command.");
+
+		// create some criteria
+		List<CriteriaRequest> requests = new ArrayList<CriteriaRequest>();
+		requests.add(new CriteriaRequest("ObjectA.id", Comparators.LESS_THAN,
+				"34"));
+		requests.add(new CriteriaRequest("ObjectB.name", Comparators.CONTAINS,
+				"stuff"));
+		requests.add(new CriteriaRequest("ObjectC.endOfTheWorld",
+				Comparators.ON, "2095-01-01"));
+
+		// fill the criteria.
+		parser.fillCriteria(crit, requests);
+
+		// verify all aliases were joined
+		verify(crit).createAlias("b", "b");
+		verify(crit).createAlias("b.endOfTheWorldCounter",
+				"endOfTheWorldCounter");
+		verify(crit, times(2)).createAlias(anyString(), anyString());
+
+		// verify we append the projections
+		verify(crit).setProjection((Projection) anyObject());
+
+		// verify that the where clauses we added
+		verify(crit, times(3)).add((Criterion) anyObject());
+	}
+
+	@Test
+	public void testRegexReplacement() {
+		String field = "thisIsATest";
+		String result = field.replaceAll("([A-Z])", " $1");
+		Assert.assertEquals("this Is A Test", result);
+	}
+
+	@Test
+	public void testDescriptionGeneration() {
+		CriteriaStructureUtil parser = new CriteriaStructureUtil();
+		Assert.assertTrue(parser.constructDescription("PatientInfo.lastName")
+				.equals("Last Name from Patient Info"));
+
+		Assert.assertTrue(parser.constructDescription(
+				"SomeReallyLongNonsense." + "aFieldWhichIsFarTooLongInName")
+				.equals(
+						"A Field Which Is Far Too Long In "
+								+ "Name from Some Really Long Nonsense"));
 	}
 
 	/**
